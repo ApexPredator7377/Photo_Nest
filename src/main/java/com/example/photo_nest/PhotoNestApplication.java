10d0ad2879e9dce73a6beb90ca6aad7a22512f82
Nathan_Morgenrood-32674309 <Nathan333nm@gmail.com>
2021-11-17 17:37:41 +0200

    Initial commit second try using IntelliJ

---
+++ src/main/java/com/example/photo_nest/PhotoNestApplication.java
@@ -0,0 +1,13 @@
+package com.example.photo_nest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoNestApplication.class, args);
+    }
+
+}
