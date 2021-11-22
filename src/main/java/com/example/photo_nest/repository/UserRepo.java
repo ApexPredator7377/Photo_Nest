0d0948b156603964f06c52c3f0b148a527465e79
Nathan_Morgenrood-32674309 <Nathan333nm@gmail.com>
2021-11-23 00:31:00 +0200

    Linked to Azure database

--- src/main/java/com/example/photo_nest/repository/UserRepo.java
+++ src/main/java/com/example/photo_nest/repository/UserRepo.java
@@ -1,5 +1,4 @@
 package com.example.photo_nest.repository;
-
 import com.example.photo_nest.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
